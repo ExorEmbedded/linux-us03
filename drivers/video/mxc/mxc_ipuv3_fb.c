e6714668095a96ac38501306c035a67a1a3de964
Giovanni Pavoni <gpavoni@Ubuntu1204-giovanni.(none)>
2016-04-13 12:30:27 +0200

    imx6usom US03 ------------------------- - Fix to framebuffer driver to effectively use the default_bpp value taken from DTB. This is required for the eTOP7xx panels, which work at 24bpp.

--- drivers/video/mxc/mxc_ipuv3_fb.c
+++ drivers/video/mxc/mxc_ipuv3_fb.c
@@ -873,7 +873,13 @@ static int mxcfb_check_var(struct fb_var_screeninfo *var, struct fb_info *info)
 	if ((var->bits_per_pixel != 32) && (var->bits_per_pixel != 24) &&
 	    (var->bits_per_pixel != 16) && (var->bits_per_pixel != 12) &&
 	    (var->bits_per_pixel != 8))
-		var->bits_per_pixel = 16;
+	{
+	  var->bits_per_pixel = mxc_fbi->default_bpp;
+	  if ((var->bits_per_pixel != 32) && (var->bits_per_pixel != 24) &&
+	    (var->bits_per_pixel != 16) && (var->bits_per_pixel != 12) &&
+	    (var->bits_per_pixel != 8))
+	    var->bits_per_pixel = 16;
+	}
 
 	if (check_var_pixfmt(var))
 		/* Fall back to default */

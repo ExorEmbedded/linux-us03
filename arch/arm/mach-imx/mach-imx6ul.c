8d3c51410f7ec82abfc81b3a49064505d7cc6fcf
Giovanni Pavoni <gpavoni@Ubuntu1204-giovanni.(none)>
2017-08-24 17:57:31 +0200

    ns01-evk: Changed tx_clock direction for the eth RMII phys; on our HW the clock is always generated by the eth phy and is an input for the imx6UL CPU.

--- arch/arm/mach-imx/mach-imx6ul.c
+++ arch/arm/mach-imx/mach-imx6ul.c
@@ -30,8 +30,11 @@ static void __init imx6ul_enet_clk_init(void)
 
 	gpr = syscon_regmap_lookup_by_compatible("fsl,imx6ul-iomuxc-gpr");
 	if (!IS_ERR(gpr))
-		regmap_update_bits(gpr, IOMUXC_GPR1, IMX6UL_GPR1_ENET_CLK_DIR,
-				   IMX6UL_GPR1_ENET_CLK_OUTPUT);
+#if 0
+	  regmap_update_bits(gpr, IOMUXC_GPR1, IMX6UL_GPR1_ENET_CLK_DIR, IMX6UL_GPR1_ENET_CLK_OUTPUT);
+#else
+	  regmap_update_bits(gpr, IOMUXC_GPR1, IMX6UL_GPR1_ENET_CLK_SEL, IMX6UL_GPR1_ENET_CLK_SEL); //We use imx6UL with clock input from RMII phys
+#endif
 	else
 		pr_err("failed to find fsl,imx6ul-iomux-gpr regmap\n");
 

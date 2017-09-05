dfdec08104c3d1a07c74ada86109654e09266e26
Giovanni Pavoni <gpavoni@Ubuntu1204-giovanni.(none)>
2017-09-05 15:42:04 +0200

    BSP-816: Added display ID=62 for JSmart10

--- include/video/displayconfig.h
+++ include/video/displayconfig.h
@@ -63,8 +63,9 @@
  *1.11			SS							20.04.2017  Added display code #59: FutureLabs  FLT-070D07-W0 800x480 High Brightness for WE16
  *																			Added display code #60: CHIMEI TG070Y2-L01 800x480 for WE16 using both US01Ax and US03Ax
  *1.12			SS							01.09.2017  Added display code #61: FutureLabs 1024x600 High Brightness for Jsmart07---Initial definition without any datasheet/spec
+ *1.13			SS							05.09.2017  Added display code #62: FutureLabs 1280x800 High Brightness for Jsmart10---Initial definition without any datasheet/spec
  *
- * NEXT AVAILABLE DISPLAY CODE: 62
+ * NEXT AVAILABLE DISPLAY CODE: 63
  */
  
 #ifndef DISPLAYCONFIG_H
@@ -714,60 +715,60 @@ static struct t_DisplayParams displayconfig[] = {
         .pwmfreq        = 250,
         .brightness_min = 20,
         .brightness_max = 100,
-    },
+    }, 
     /* 59: FutureLabs  FLT-070D07-W0 800x480 High Brightness*/
     {
         .dispid    = 59,
-        .rezx      = 800,
-        .rezy      = 480,
+        .rezx      = 800, 
+        .rezy      = 480, 
         .bpp       = 16,
-
-        .pclk_freq = 30000,
+        
+        .pclk_freq = 30000, 
         .pclk_inv  = 1,	//20.04.2017 inverted clock polarity due to IMX.6 bug; for US01Ax TTL will drive the display lines on falling edge => LVDS driver needs to sample on rising edge
-
-        .hs_fp     = 41,
-        .hs_bp     = 35,
-        .hs_w      = 129,
+        
+        .hs_fp     = 41, 
+        .hs_bp     = 35, 
+        .hs_w      = 129, 
         .hs_inv    = 1,
-
-        .vs_fp     = 12,
-        .vs_bp     = 35,
-        .vs_w      = 3,
+        
+        .vs_fp     = 12, 
+        .vs_bp     = 35, 
+        .vs_w      = 3, 
         .vs_inv    = 1,
-
+        
         .blank_inv      = 0,
-
+        
         .pwmfreq        = 10000,  //20.04.2017 backlight driven by TPS61165
         .brightness_min = 1,
         .brightness_max = 100,
-    },
+    }, 
     /* 60: CHIMEI TG070Y2-L01 800x480 for WE16 using both US01Ax and US03Ax*/
     {
         .dispid    = 60,
-        .rezx      = 800,
-        .rezy      = 480,
+        .rezx      = 800, 
+        .rezy      = 480, 
         .bpp       = 16,
-
-        .pclk_freq = 30000,
+        
+        .pclk_freq = 30000, 
         .pclk_inv  = 1,	//20.04.2017 inverted clock polarity due to IMX.6 bug; for US01Ax TTL will drive the display lines on falling edge => LVDS driver needs to sample on rising edge
-
-        .hs_fp     = 41,
-        .hs_bp     = 35,
-        .hs_w      = 129,
+        
+        .hs_fp     = 41, 
+        .hs_bp     = 35, 
+        .hs_w      = 129, 
         .hs_inv    = 1,
-
-        .vs_fp     = 12,
-        .vs_bp     = 35,
-        .vs_w      = 3,
+        
+        .vs_fp     = 12, 
+        .vs_bp     = 35, 
+        .vs_w      = 3, 
         .vs_inv    = 1,
-
+        
         .blank_inv      = 0,
-
-        .pwmfreq        = 200,
+        
+        .pwmfreq        = 200,  
         .brightness_min = 20,
         .brightness_max = 100,
     }, 
-    /* 61: FutureLabs Jsmart07 1024x600*/
+    /* 61: FutureLabs Jsmart07 1024x600 IMX.6 ONLY*/
     {
         .dispid    = 61,
         .rezx      = 1024, 
@@ -775,7 +776,7 @@ static struct t_DisplayParams displayconfig[] = {
         .bpp       = 24,
         
         .pclk_freq = 51000, 
-        .pclk_inv  = 1,  //20.04.2017 inverted clock polarity due to IMX.6 bug; for US01Ax TTL will drive the display lines on falling edge => LVDS driver needs to sample on rising edge
+        .pclk_inv  = 1,  // inverted clock polarity due to IMX.6 bug
         
         .hs_fp     = 10, 
         .hs_bp     = 320, 
@@ -792,7 +793,33 @@ static struct t_DisplayParams displayconfig[] = {
         .pwmfreq        = 10000,
         .brightness_min = 10,
         .brightness_max = 100,
-    },                          
+    },
+    /* 62: FutureLabs Jsmart10 LVDS 24 bit 1280x800 IMX.6 ONLY */
+    {
+        .dispid    = 62,
+        .rezx      = 1280, 
+        .rezy      = 800, 
+        .bpp       = 24,
+        
+        .pclk_freq = 71100, 
+        .pclk_inv  = 1,		// inverted clock polarity due to IMX.6 bug
+        
+        .hs_fp     = 30, 
+        .hs_bp     = 30, 
+        .hs_w      = 100, 
+        .hs_inv    = 0,
+        
+        .vs_fp     = 3, 
+        .vs_bp     = 10, 
+        .vs_w      = 10, 
+        .vs_inv    = 0,
+        
+        .blank_inv      = 0,
+        
+        .pwmfreq        = 10000,
+        .brightness_min = 10,
+        .brightness_max = 100,
+    },                                        
     /* END OF LIST */
     {
       .dispid    = NODISPLAY,

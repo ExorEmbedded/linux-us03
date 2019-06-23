19615408b2b026c738f9078e5c53fe14ef3f1ec9
Luigi Scagnet - Exorint <luigi.scagnet@exorint.com>
2019-06-23 19:58:11 +0200

    BSP-1356: US03 max cpu frequency clamped to 800Mhz

--- arch/arm/mach-imx/mach-imx6q.c
+++ arch/arm/mach-imx/mach-imx6q.c
@@ -374,12 +374,12 @@ static void __init imx6q_opp_check_speed_grading(struct device *cpu_dev)
 	val >>= OCOTP_CFG3_SPEED_SHIFT;
 	val &= 0x3;
 
-	if ((val != OCOTP_CFG3_SPEED_1P2GHZ) && cpu_is_imx6q())
-		if (dev_pm_opp_disable(cpu_dev, 1200000000))
-			pr_warn("failed to disable 1.2 GHz OPP\n");
-	if (val < OCOTP_CFG3_SPEED_996MHZ)
-		if (dev_pm_opp_disable(cpu_dev, 996000000))
-			pr_warn("failed to disable 996 MHz OPP\n");
+	//Force disabling 1.2Ghz OPP -- if ((val != OCOTP_CFG3_SPEED_1P2GHZ) && cpu_is_imx6q())
+	if (dev_pm_opp_disable(cpu_dev, 1200000000))
+		pr_warn("failed to disable 1.2 GHz OPP\n");
+	//Force disabling 1.2Ghz OPP -- if (val < OCOTP_CFG3_SPEED_996MHZ)
+	if (dev_pm_opp_disable(cpu_dev, 996000000))
+		pr_warn("failed to disable 996 MHz OPP\n");
 	if (cpu_is_imx6q()) {
 		if (val != OCOTP_CFG3_SPEED_852MHZ)
 			if (dev_pm_opp_disable(cpu_dev, 852000000))

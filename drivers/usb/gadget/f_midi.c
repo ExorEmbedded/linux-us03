05472a85182af7cc820736b844b42797c3054635
Nicolò Ongaro <nicolo.ongaro@exorembedded.net>
2020-02-12 14:38:12 +0100

    BSP-1773 CVE-2018-20961, fix double free vulnerability in the f_midi_set_alt function

--- drivers/usb/gadget/f_midi.c
+++ drivers/usb/gadget/f_midi.c
@@ -201,7 +201,9 @@ static inline struct usb_request *midi_alloc_ep_req(struct usb_ep *ep,
 
 static void free_ep_req(struct usb_ep *ep, struct usb_request *req)
 {
+	WARN_ON(req->buf == NULL);
 	kfree(req->buf);
+	req->buf = NULL;
 	usb_ep_free_request(ep, req);
 }
 
@@ -366,6 +368,9 @@ static int f_midi_set_alt(struct usb_function *f, unsigned intf, unsigned alt)
 		if (err) {
 			ERROR(midi, "%s queue req: %d\n",
 				    midi->out_ep->name, err);
+			if (req->buf != NULL)
+				free_ep_req(midi->out_ep, req);
+			return err;
 		}
 	}
 

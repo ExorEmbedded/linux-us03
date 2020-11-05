7b7254fb9c69bf39577cac3d92196592da529048
Giovanni Pavoni - Exorint <giovanni.pavoni@exorint.com>
2020-11-05 15:50:47 +0100

    BSP-2386: EMMC driver fixes

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -1203,7 +1203,7 @@ static void sdhci_esdhc_imx_hwinit(struct sdhci_host *host)
 		* erratum ESDHC_FLAG_ERR004536 fix for MX6Q TO1.2 and MX6DL
 		* TO1.1, it's harmless for MX6SL
 		*/
-		writel(readl(host->ioaddr + 0x6c) | BIT(7),
+		writel(readl(host->ioaddr + 0x6c) & ~BIT(7),
 			host->ioaddr + 0x6c);
 
 		/* disable DLL_CTRL delay line settings */

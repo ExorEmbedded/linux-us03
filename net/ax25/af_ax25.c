ae29bca68355a84aee98280bcfe51c798e12e721
Nicolò Ongaro <nicolo.ongaro@exorint.com>
2022-09-19 10:50:06 +0200

    BSP-3048 CVE-2022-1204 mark affected code

--- net/ax25/af_ax25.c
+++ net/ax25/af_ax25.c
@@ -1,4 +1,4 @@
-#error CVE-2022-1199
+#error CVE-2022-1199 CVE-2022-1204
 /*
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

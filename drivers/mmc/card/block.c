150f6c61423dec90319e34cb612879a2bec31891
Giovanni Pavoni <gpavoni@Ubuntu1204-giovanni.(none)>
2022-11-10 09:59:43 +0100

    BSP-4472 US03/US04: fix for random SW restart issues with DG4008 8GB Sandisk EMMC

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -2319,6 +2319,7 @@ force_ro_fail:
 #define CID_MANFID_TOSHIBA	0x11
 #define CID_MANFID_MICRON	0x13
 #define CID_MANFID_SAMSUNG	0x15
+#define CID_MANFID_SANDISKWD    0x45
 
 static const struct mmc_fixup blk_fixups[] =
 {
@@ -2376,6 +2377,16 @@ static const struct mmc_fixup blk_fixups[] =
 		  MMC_QUIRK_SEC_ERASE_TRIM_BROKEN),
 	MMC_FIXUP("VZL00M", CID_MANFID_SAMSUNG, CID_OEMID_ANY, add_quirk_mmc,
 		  MMC_QUIRK_SEC_ERASE_TRIM_BROKEN),
+		
+	
+	/*
+	 * On some Sandisk/WD EMMC devices SW restart issues were randomly
+	 * noticed: the fix consists in sending an MMC GO PRE IDLE cmd
+	 * just before resetting the CPU
+	 */
+	MMC_FIXUP("DG4008", CID_MANFID_SANDISKWD, CID_OEMID_ANY, add_quirk_mmc,
+		  MMC_QUIRK_SWRESTART),
+      
 
 	END_FIXUP
 };
